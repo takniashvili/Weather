34bbadd00932003fa19509867b7b84f0bf340d6b
BlaCkTy <104572174+GentleCow@users.noreply.github.com>
2023-10-18 18:56:28 +0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,25 +1,8 @@
-// IF SONG IS NOT AUTOPLAY THIS VERSION BEETTER
-
-// var audio = document.getElementById('audio');
-// var playPauseBTN = document.getElementById('playPauseBTN');
-// var count = 0;
-
-// function playPause () {
-//   if(count == 0) {
-//     count = 1;
-//     audio.play();
-//     playPauseBTN.innerHTML = "Stop | |";
-//   } else {
-//     count = 0;
-//     audio.pause();
-//     playPauseBTN.innerHTML = "Play &#9655;";
-//   }
-// }
-
 var audio = document.getElementById('audio');
 var playPauseBTN = document.getElementById('playPauseBTN');
 var count = 1; // Start with count = 1 to indicate "Pause" initially
 
+// Function to play or pause the audio
 function playPause() {
   if (count === 1) {
     count = 0;
@@ -32,9 +15,20 @@ function playPause() {
   }
 }
 
+// Function to restart the audio when it ends
+function restartAudio() {
+  audio.currentTime = 0; // Set the audio's current time back to the beginning
+  audio.play();
+}
+
 // Initialize the button text based on autoplay
 if (audio.autoplay) {
   playPauseBTN.innerHTML = "Stop | |";
 } else {
   playPauseBTN.innerHTML = "Play &#9658;";
 }
+
+// Add an event listener to restart the audio when it ends
+audio.addEventListener('ended', function () {
+  restartAudio();
+});
